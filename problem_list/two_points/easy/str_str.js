c0f28dd2886c6ed79d117cd19d98542fc2c0ca8b
oleg sukharev <olegsukharev1991@gmail.com>
2023-08-10 11:16:47 +0200

    strStr

---
+++ problem_list/two_points/easy/str_str.js
@@ -0,0 +1,4 @@
+// Given two strings needle and haystack, return the index of the first 
+// occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+const strStr = (haystack, needle) => haystack.indexOf(needle);
