f8dda52cea3d5e26f01fb698f6303c948cdc4efa
oleg sukharev <olegsukharev1991@gmail.com>
2023-08-13 12:10:35 +0200

    lengthOfLongestSubstring  emxaple

--- problem_list/array_and_hash/medium/longest_unique_substring.js
+++ problem_list/array_and_hash/medium/longest_unique_substring.js
@@ -1,42 +1,47 @@
 // Given a string s, find the length of the longest 
 // substring without repeating characters.
 // Sliding Window
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
 
 const lengthOfLongestSubstring = (s) => {
-    let charMap = new Map();
-    let max = 0;
-    let left = 0;
+  let charMap = new Map();
+  let max = 0;
+  let left = 0;
 
-    for (let right = 0; right < s.length; right++) {
-        if (charMap.has(s[right])) {
-            left = Math.max(left, charMap.get(s[right]) + 1);
-        }
-        charMap.set(s[right], right);
-        max = Math.max(max, right - left + 1);
+  for (let right = 0; right < s.length; right++) {
+    if (charMap.has(s[right])) {
+      left = Math.max(left, charMap.get(s[right]) + 1);
     }
+    charMap.set(s[right], right);
+    max = Math.max(max, right - left + 1);
+  }
 
-    return max;
+  return max;
 };
 
 const lengthOfLongestSubstringAlt = (s) => {
-    // too long Runtime
-    let maxLength = 0;
+  // too long Runtime
+  let maxLength = 0;
 
-    for (let i = 0; i < s.length; i++) {
-        const substring = s.substring(i);
-        console.log(substring);
+  for (let i = 0; i < s.length; i++) {
+    const substring = s.substring(i);
+    console.log(substring);
 
-        for (let j = 0; j < substring.length; j++) {
-            const char = substring[j];
-            if (substring.indexOf(char) === j) {
-                if (maxLength <= j) {
-                    maxLength = j + 1;
-                }
-            } else {
-                break;
-            }
+    for (let j = 0; j < substring.length; j++) {
+      const char = substring[j];
+      if (substring.indexOf(char) === j) {
+        if (maxLength <= j) {
+          maxLength = j + 1;
         }
+      } else {
+        break;
+      }
     }
-    return maxLength;
+  }
+  return maxLength;
 };
 
