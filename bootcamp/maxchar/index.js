bb4ffceacb61efe04b58ebc758d7d0eb5fb44e19
oleg sukharev <olegsukharev1991@gmail.com>
2023-11-01 20:11:50 +0100

    axChar

--- bootcamp/maxchar/index.js
+++ bootcamp/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const map = new Map();
+  let maxChar = '';
+  let maxCount = 0;
+
+  for (const char of str) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+
+  for (const [char, count] of map) {
+    if (count > maxCount) {
+      maxCount = count;
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
