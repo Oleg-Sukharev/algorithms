8326fc81cdfe2aac6c6caeddc88cc4a74d5e3e18
oleg sukharev <olegsukharev1991@gmail.com>
2023-11-02 19:04:58 +0100

    chunk

--- bootcamp/chunk/index.js
+++ bootcamp/chunk/index.js
@@ -8,6 +8,27 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//   const chunked = [];
+
+//   for (let i = 0; i < array.length; i += size) {
+//     const subArray = array.slice(i, i + size);
+//     chunked.push(subArray);
+//   }
+
+//   return chunked;
+// }
+
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;
