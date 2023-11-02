8636a5e5b10a0bd350dc7b112524beecffc38e0f
oleg sukharev <olegsukharev1991@gmail.com>
2023-11-02 21:23:15 +0100

    chunk alternative

--- bootcamp/chunk/index.js
+++ bootcamp/chunk/index.js
@@ -19,16 +19,33 @@
 //   return chunked;
 // }
 
+// !important
 function chunk(array, size) {
   const chunked = [];
-  let index = 0;
+  for (const element of array) {
+    console.log(element);
+    const last = chunked[chunked.length - 1];
 
-  while (index < array.length) {
-    chunked.push(array.slice(index, index + size));
-    index += size;
+    if (!last || last.length === size) {
+      chunked.push([element])
+    } else {
+      last.push(element);
+    }
   }
 
   return chunked;
 }
+
+// function chunk(array, size) {
+//   const chunked = [];
+//   let index = 0;
+
+//   while (index < array.length) {
+//     chunked.push(array.slice(index, index + size));
+//     index += size;
+//   }
+
+//   return chunked;
+// }
 
 module.exports = chunk;
