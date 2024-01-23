d624b4ccd46620c7deec579bceef2ff3fa5501ed
oleg sukharev <olegsukharev1991@gmail.com>
2024-01-23 21:29:49 +0100

    adding removeLast and insertLast methods to linked list

--- bootcamp/linkedlist/index.js
+++ bootcamp/linkedlist/index.js
@@ -66,6 +66,36 @@ class LinkedList {
     this.head = this.head.next;
   }
 
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let previous = this.head;
+    let node = this.head.next;
+
+    while (node.next) {
+      previous = node;
+      node = node.next
+    }
+
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+
+    if (last) {
+      last.next = new Node(data);
+    } else {
+      this.head = new Node(data);
+    }
+  }
 }
 
 const list = new LinkedList();
