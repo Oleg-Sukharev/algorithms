db90a854c0b3faf517f26c0bc92063db316f67af
oleg sukharev <olegsukharev1991@gmail.com>
2024-02-01 20:42:12 +0100

    traverseBF

--- bootcamp/tree/index.js
+++ bootcamp/tree/index.js
@@ -11,27 +11,37 @@
 // function that gets called with each element in the tree
 
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.children = [];
-    }
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
-    add(data) {
-        const node = new Node(data);
-        this.children.push(node);
-    }
+  add(data) {
+    const node = new Node(data);
+    this.children.push(node);
+  }
 
-    remove(removeData) {
-        this.children = this.children.filter(
-            ({ data }) => removeData !== data
-        );
-    }
+  remove(removeData) {
+    this.children = this.children.filter(
+      ({ data }) => removeData !== data
+    );
+  }
 }
 
 class Tree {
-    constructor() {
-        this.root = null;
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    const arr = [this.root];
+    while (arr.length) {
+      const node = arr.shift();
+
+      arr.push(...node.children);
+      fn(node);
     }
+  }
 }
 
 module.exports = { Tree, Node };
