6d91277a7b3eadf43904d6da71846c3411de0319
oleg sukharev <olegsukharev1991@gmail.com>
2024-03-17 20:37:42 +0100

    reverseInt

--- bootcamp/reverseint/index.js
+++ bootcamp/reverseint/index.js
@@ -8,6 +8,29 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+// The Math.sign() static method returns 1 or -1, 
+// indicating the sign of the number passed as argument. If the input is 0 or -0, it will be returned as-is.
+console.log(Math.sign(3));
+// Expected output: 1
+
+console.log(Math.sign(-3));
+// Expected output: -1
+
+console.log(Math.sign(0));
+// Expected output: 0
+
+console.log(Math.sign('-3'));
+// Expected output: -1
+
