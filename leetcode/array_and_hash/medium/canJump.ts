5768968f5484c548c1c957a133138c23c5417ea8
oleg <olegsukharev1991@gmail.com>
2025-01-05 04:06:45 +0100

    canJump

---
+++ leetcode/array_and_hash/medium/canJump.ts
@@ -0,0 +1,28 @@
+// You are given an integer array nums.
+// You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+// Return true if you can reach the last index, or false otherwise.
+
+//   Example 1:
+// Input: nums = [2, 3, 1, 1, 4]
+// Output: true
+// Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+function canJump(nums: number[]): boolean {
+  if (nums.length <= 1) return true;
+  let maxReach = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    // If the current index is beyond the max reachable index, return false
+    if (i > maxReach) {
+      return false;
+    }
+    // Update the maximum reachable index
+    maxReach = Math.max(maxReach, i + nums[i]);
+    // If we can reach or exceed the last index, return true
+    if (maxReach >= nums.length - 1) {
+      return true;
+    }
+  }
+
+  return false;
+};
